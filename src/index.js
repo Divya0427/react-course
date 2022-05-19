f932b006426f3247b7b64dc19e1ee2dadaaab892
DivyaUI <Divya0427@users.noreply.github.com>
2022-05-19 12:59:44 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/* 
+- starting point to our react application
+- For js files, we can omit the extension while importing
+
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { FavoritesContextProvider } from './store/favorites-context';
+
+ReactDOM.render(
+    <FavoritesContextProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </FavoritesContextProvider>,
+    document.getElementById('root')
+);
